514c20ab9148fab9c96308e2b41d8b007b957e21
sophielrt <sophielouiserose@icloud.com>
2025-12-10 10:33:23 +0000

    installed gunicorn and created procfile for deployment

--- the_independent_explorer/settings.py
+++ the_independent_explorer/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0uyzlv*moj)wid#y+3fp)!dl+hv6vthacdrza0o=w=#ko%^p*r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
