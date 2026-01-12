c43e3c329e90112426d852620b96334b97bc649b
sophielrt <sophielouiserose@icloud.com>
2026-01-12 15:15:04 +0000

    Editted blog models as destinations had wrong field type, it has now been updated to CharField which is correct

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     Slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     read_time = models.IntegerField(help_text="Estimated read time in minutes")
-    destination = models.IntegerField(help_text="Destination in which blog post is related to")
+    destination = models.CharField(max_length=200, help_text="Destination in which blog post is related to")
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
