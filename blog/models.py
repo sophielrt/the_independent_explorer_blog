b15aed80c9973c508da4dec8cc906d092567aa07
sophielrt <sophielouiserose@icloud.com>
2026-01-12 15:45:50 +0000

    Added blog comment models

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     Slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     read_time = models.IntegerField(help_text="Estimated read time in minutes")
-    destination = models.CharField(max_length=200, help_text="Destination in which blog post is related to")
+    destination = models.CharField(max_length=200, help_text="Destination which the blog post is related to")
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -18,4 +18,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     published = models.DateTimeField(blank=True, null=True)
-    view_count = models.IntegerField(default=0)
+    view_count = models.IntegerField(default=0) # Need to edit this field to increment views automatically!
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
