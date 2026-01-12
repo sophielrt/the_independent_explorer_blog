17ab9d1e97ad5e09a4e56d22f18269bdbb6396a9
sophielrt <sophielouiserose@icloud.com>
2026-01-12 16:03:06 +0000

    Adding methods to comment and blog model to determin layout and display in database

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,11 @@ class Post(models.Model):
     published = models.DateTimeField(blank=True, null=True)
     view_count = models.IntegerField(default=0) # Need to edit this field to increment views automatically!
 
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.title}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
@@ -27,3 +32,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
