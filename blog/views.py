1d20940ae52a757aede08189ddb8e2922a3dff30
sophielrt <sophielouiserose@icloud.com>
2026-04-26 14:33:30 +0100

    added pagination to blog posts, added previous page button and next page button, for blog posts on index.html.

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,5 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = 'blog/index.html'
-    paginate_by = 4
+    paginate_by = 4 # 4 posts per page = 2 rows of 2
+    ordering = ["published"]
