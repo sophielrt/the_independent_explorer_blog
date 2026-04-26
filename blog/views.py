e115284a9dabdc9e540ceb3cfbfe4c95a0d7cde1
sophielrt <sophielouiserose@icloud.com>
2026-04-26 16:49:09 +0100

    added blog post view and post_details.html so that the blog preview in index.html can be opened.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from requests import post
 from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = 'blog/index.html'
+    template_name = "blog/index.html"
     paginate_by = 4 # 4 posts per page = 2 rows of 2
     ordering = ["published"]
+
+def post_detail(request, slug):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(request, "blog/post_detail.html", {"post": post},)
