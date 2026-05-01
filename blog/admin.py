d2666afe423e2f6b57a049562cc26a00eea1813f
sophielrt <sophielouiserose@icloud.com>
2026-05-01 17:05:30 +0100

    removed comments models

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -13,4 +13,4 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Comment)
+
