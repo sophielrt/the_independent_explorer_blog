734406f83662bff8b8f18cbe1c9e9aab440fc79c
sophielrt <sophielouiserose@icloud.com>
2026-05-01 17:52:42 +0100

    Added favicon logo to browser tab, added logo to navbar added size styling in css. Added information about technology used and credits in read.me file

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('admin-dashboard/add/', views.post_add, name='post_add'),
     path('admin-dashboard/edit/<int:pk>/', views.post_edit, name='post_edit'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/logo.ico'), permanent=True)),
 ]
