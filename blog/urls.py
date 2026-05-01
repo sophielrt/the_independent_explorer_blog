5172991e816c5b17e7a38ebf388bc71379aaf86e
sophielrt <sophielouiserose@icloud.com>
2026-05-01 12:26:12 +0100

    Added a login for the admin to login into a dashboard

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
+    path('admin-login/', views.admin_login, name='admin_login'),
+    path('admin-logout/', views.admin_logout, name='admin_logout'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views import generic
-from requests import post
 from .models import Post
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -14,3 +14,19 @@ def post_detail(request, slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     return render(request, "blog/post_detail.html", {"post": post},)
+
+def admin_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None and user.is_staff:
+            login(request, user)
+            return redirect("/")
+        else:
+            return render(request, "blog/admin_login.html", {"error": "Invalid credentials or not an admin."})
+    return render(request, "blog/admin_login.html")
+
+def admin_logout(request):
+    logout(request)
+    return redirect("admin_login")
