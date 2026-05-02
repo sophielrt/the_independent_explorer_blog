171bdc1b49c4ff78641e996e337c82d04928b1ab
sophielrt <sophielouiserose@icloud.com>
2026-05-02 16:56:57 +0100

    fix whitenoise storage settings

--- the_independent_explorer/settings.py
+++ the_independent_explorer/settings.py
@@ -147,6 +147,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
